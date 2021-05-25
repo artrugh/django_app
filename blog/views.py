76e9d4971d28aa747a91599d5dc760d76a598272
artrugh <arturo.rugh@gmail.com>
2021-05-26 00:42:43 +0200

    add-template

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return HttpResponse("Hey there")
+    posts = Post.objects.all()
+    print(posts)
+    return render(request, 'index.html', {"posts": posts})
 
 
 def post(request):
