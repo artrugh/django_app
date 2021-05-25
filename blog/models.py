b83ff8149fa6da1de455bea2f584c9da4ea1cdf0
artrugh <arturo.rugh@gmail.com>
2021-05-25 23:46:33 +0200

    add-blog-model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    # inputs
+    title = models.CharField(max_length=255)
+    summary = models.CharField(max_length=300)
+
+    slug = models.SlugField(max_length=255, unique=True)
+    content = models.TextField()
+
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+        def __unicode__(self):
+            return u'%s' % self.title
+
+    def get_absolute_url(self):
+        return reverse("blog.views.post", args=[self.slug])
